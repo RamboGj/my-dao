19cf5d40ff218aae11f8104cb7cb5cdc8e4cf147
João Rambo <jpramboguanabara@hotmail.com>
2022-08-01 13:01:51 -0300

    first commit scripts 1 and 2

---
+++ scripts/2-deploy-drop.js
@@ -0,0 +1,45 @@
+import { AddressZero } from "@ethersproject/constants";
+import sdk from "./1-initialize-sdk.js";
+import { readFileSync } from "fs";
+
+(async () => {
+    try {
+      const editionDropAddress = await sdk.deployer.deployEditionDrop({
+  
+        // O nome da coleção, ex. CryptoPunks
+        name: "Membro da FreedomDAO",
+  
+        // Uma descrição para a coleção.
+        description: "Uma DAO para sermos livres",
+  
+        // Uma imagem para a coleção que vai aparecer no OpenSea.
+        image: readFileSync("scripts/assets/download.png"),
+
+        // Nós precisamos passar o endereço da pessoa que vai estar recebendo os rendimentos das vendas dos nfts do módulo.
+        // Nós estamos planejando não cobrar as pessoas pelo drop, então passaremos o endereço 0x0
+        // você pode configurar isso para sua própria carteira se você quiser cobrar pelo drop.
+          primary_sale_recipient: AddressZero,
+      });
+  
+      // essa inicialização retorna o endereço do nosso contrato
+      // usamos para inicializar o contrato no sdk
+      const editionDrop = sdk.getEditionDrop(editionDropAddress);
+
+      // com isso, temos os metadados no nosso contrato
+      const metadata = await editionDrop.metadata.get();
+  
+      console.log(
+        "✅ Contrato editionDrop implantado com sucesso, endereço:",
+        editionDropAddress,
+      );
+  
+      console.log(
+        "✅ bundleDrop metadados:",
+        metadata,
+      );
+  
+    } catch (error) {
+      console.log("falha ao implantar contrato editionDrop", error);
+    }
+})()
+  
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useAddress, useMetamask } from '@thirdweb-dev/react';
+
 const App = () => {
-  return (
-    <div className="landing">
-      <h1>Bem-vind@s à minha DAO</h1>
-    </div>
-  )
+  const address = useAddress()
+  const connectWithMetamask = useMetamask()
+  
+  console.log("👋 Address:", address);
+  
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Bem-vind@s à FreedomDAO</h1>
+        <button
+          onClick={connectWithMetamask}
+          className="btn-hero"  
+        >
+          Conecte sua carteira
+        </button>
+        <p>Junte-se à minha DAO</p>
+      </div>
+    )
+  } else {
+    return (
+      <div className="landing">
+        <h1>👀 carteira conectada, e agora?!</h1>
+      </div>
+    )
+  }
+  
 }
 
 export default App
